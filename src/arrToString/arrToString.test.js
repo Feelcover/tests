541de7973ad0b4282bf80b9c89e0abcbb46938f6
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-26 21:50:28 +0300

    тесты для arrToString

--- src/arrToString/arrToString.test.js
+++ src/arrToString/arrToString.test.js
@@ -3,5 +3,14 @@ const arrToString = require("./arrToString");
 describe("arrToString", () => {
   test("корректное значение", () => {
     expect(arrToString([1, 2, 3, 4, 5])).toEqual(["1", "2", "3", "4", "5"]);
-  });
+  }),
+  test("невалидные значения", () => {
+    expect(arrToString([1, 2, 3, 4, 5])).toEqual(["1", "2", "3", "4", "5"]);
+  }),
+  test("пустой массив", () => {
+    expect(arrToString([])).toEqual([]);
+  }),
+  test("пустой массив", () => {
+    expect(arrToString([1,2,3])).not.toEqual(["1","2","3","4"]);
+  })
 });
