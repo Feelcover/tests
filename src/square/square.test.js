b4d4c05f6b698b570db09915189d3d15254a3ae4
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-03-26 22:30:06 +0300

    тесты для square

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -4,14 +4,25 @@ describe("square", () => {
   //перед каждым тестом
   beforeEach(() => {});
   //единоразово
-  beforeEachAll(() => {});
+  //   beforeEachAll(() => {});
   test("корректное значение", () => {
-    expect(square(2)).toBe(4);
-  }),
-    test("корректное значение", () => {
-      expect(square(2)).toBeLessThan(5);
-    }),
-    test("корректное значение", () => {
-      expect(square(2)).toBeGreaterThan(3);
-    });
+    //     expect(square(2)).toBe(4);
+    //       expect(square(2)).toBeLessThan(5);
+    //       expect(square(2)).toBeGreaterThan(3);
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(2);
+    expect(spyMathPow).toBeCalledTimes(1);
+  });
+  test("дополнительный", () => {
+    //     expect(square(2)).toBe(4);
+    //       expect(square(2)).toBeLessThan(5);
+    //       expect(square(2)).toBeGreaterThan(3);
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(1);
+    expect(spyMathPow).toBeCalledTimes(0);
+  });
+  //после каждого теста
+  afterEach(() => {jest.clearAllMocks()});
+  //после каждого теста
+  //   afterEachAll(() => {})
 });
